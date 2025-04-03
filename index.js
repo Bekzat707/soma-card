43ae988f77eadeff70328ef3041ac7d6ea0a8216
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-04-03 23:10:26 +0500

    have some issues with index.html

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const currentLang = document.getElementById('currentLang');
     const langDropdown = document.getElementById('langDropdown');
     const langOptions = document.querySelectorAll('.lang-option');
 
- 
+  
     currentLang.addEventListener('click', () => {
         langDropdown.classList.toggle('show');
     });
 
+    
     langOptions.forEach(option => {
         option.addEventListener('click', () => {
             const lang = option.dataset.lang;
@@ -19,32 +19,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    
+
     document.addEventListener('click', (e) => {
         if (!e.target.closest('.language-selector')) {
             langDropdown.classList.remove('show');
         }
     });
     
-   
+    
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-            
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
         });
     });
 
- 
+  
+    function changeLanguage(lang) {
+        console.log(`Смена языка на: ${lang}`);
+        
+    }
 });
-
-
-function changeLanguage(lang) {
-   
-    console.log(`Changing language to: ${lang}`);
-    
-    
-}
-
